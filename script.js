c0ade905362d9c2ba38bba934ac64e21ca27d499
Tushar Sharma <145897973+Tushar161616@users.noreply.github.com>
2023-12-26 19:16:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+function genran() {
+let num = 	parseInt(Math.random()*100);
+	if(num < 47)
+	{
+		num+=47;
+	}
+	return num;
+}
 function makeid(l) {
   // write your code here
+	let str = "";
+	for(let i = 0; i<l; i++)
+		{
+	str = str + String.fromCharCode(genran());
+		}
+	return str;
 }
 
 // Do not change the code below.
